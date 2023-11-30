7c46877c449019980fe07b102561522312960fd4
Jean-Denis Boivin <jd@boiv.in>
2023-11-30 17:32:36 -0500

    Fix error that break the drawing

--- HierarchyDecorator/Scripts/Editor/Hierarchy/Drawers/BreadcrumbsDrawer.cs
+++ HierarchyDecorator/Scripts/Editor/Hierarchy/Drawers/BreadcrumbsDrawer.cs
@@ -34,6 +34,7 @@ namespace HierarchyDecorator
             GlobalData data = _settings.globalData;
 
             Transform transform = current.Transform;
+            if (transform == null) return;
 
             int depth = current.CalculateDepth();
 
